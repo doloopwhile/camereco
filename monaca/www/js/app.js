e6256f41d1ee0cbaddb93e8be7ba4997f91e39a1
OMOTO Kenji <doloopwhile@gmail.com>
2014-06-08 20:48:51 +0900

    updated

--- monaca/www/js/app.js
+++ monaca/www/js/app.js
@@ -1,1 +1,13 @@
-angular.module('myApp', ['onsen.directives']);
+var myApp = angular.module('myApp', ['onsen.directives']);
+
+myApp.filter('formatDuration', function() {
+        return function(milliseconds) {
+            if (milliseconds > 6000) {
+                milliseconds = 6000;
+            }
+            
+            var s = Math.floor(milliseconds / 1000);
+            var f = Math.floor((milliseconds % 1000) / 100);
+            return s +  "." + f;
+        }
+    });
--- monaca/www/js/page1.js
+++ monaca/www/js/page1.js
@@ -1,6 +1,7 @@
-// This is a JavaScript file
+
 function Page1Controller($scope){
     $scope.imageData = "";
+    $scope.recTime = 0;
      $scope.takePhoto = function(){
         navigator.camera.getPicture(
             function(imageData) {
@@ -14,5 +15,52 @@ function Page1Controller($scope){
                 destinationType: Camera.DestinationType.DATA_URL
             }
         );
+    }
+    $scope.recordVoice = function() {
+        var src = "myrecording.amr";
+        function onSuccess() {
+            console.log("recordAudio():Audio Success");
+        }
+        function onError(error) {
+            alert('コード: '        + error.code    + '\n' +
+                  'メッセージ: '    + error.message + '\n');
+        }
+
+
+        var mediaRec = new Media(src, onSuccess, onError);
+
+        // オーディオの録音
+        mediaRec.startRecord();
+
+        // 6秒後に録音を停止
+        var start = new Date;
+        var recInterval = setInterval(function() {
+            var now = new Date;
+            var duration = now.getTime() - start.getTime();
+            
+            $scope.$apply(function(){
+                $scope.recTime = duration;
+            });
+            
+            if (duration / 1000  >= 6) {
+                clearInterval(recInterval);
+                mediaRec.stopRecord();
+            }
+        }, 100);
+    };
+
+    
+    $scope.playVoice = function() {
+        var src = "myrecording.amr";
+          function onSuccess() {
+            console.log("recordAudio():Audio Success");
+        }
+        function onError(error) {
+            alert('コード: '        + error.code    + '\n' +
+                  'メッセージ: '    + error.message + '\n');
+        }
+
+        var media = new Media(src, onSuccess, onError);
+        media.play();
     }
 }
