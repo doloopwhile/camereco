eeb7e695958ce778899c50d688cab9055415c88d
OMOTO Kenji <doloopwhile@gmail.com>
2014-06-09 01:06:08 +0900

    updated

--- monaca/www/js/app.js
+++ monaca/www/js/app.js
@@ -1,13 +1,28 @@
 var myApp = angular.module('myApp', ['onsen.directives']);
 
 myApp.filter('formatDuration', function() {
-        return function(milliseconds) {
-            if (milliseconds > 6000) {
-                milliseconds = 6000;
-            }
-            
-            var s = Math.floor(milliseconds / 1000);
-            var f = Math.floor((milliseconds % 1000) / 100);
-            return s +  "." + f;
-        }
-    });
+  return function(milliseconds) {
+    if (milliseconds > 6000) {
+      milliseconds = 6000;
+    }
+    var s = Math.floor(milliseconds / 1000);
+    var f = Math.floor((milliseconds % 1000) / 100);
+    return s +  "." + f;
+  }
+});
+
+function db() {
+  return window.openDatabase("Database", "1.0", "Photo", 200000);
+}
+
+myApp.run(function() { 
+  db().transaction(
+    function(tx) {
+      //tx.executeSql('DROP TABLE IF EXISTS Photos');
+      //tx.executeSql('CREATE TABLE Photos (id INTEGER PRIMARY KEY, photo_data, date)');
+    },
+    function(err) {
+      alert("SQL 実行中にエラーが発生しました: "+err);
+    }
+  );
+});
--- monaca/www/js/page1.js
+++ monaca/www/js/page1.js
@@ -1,66 +1,57 @@
+function savePhoto(photoData, date) {
+  db().transaction(
+    function(tx) {
+      tx.executeSql(
+        'INSERT INTO Photos (photo_data, date) VALUES (?, ?)',
+        [photoData, date]
+      );
+    },
+    function(err) {
+      alert("SQL 実行中にエラーが発生しました: "+err);
+    }
+  );  
+}
 
 function Page1Controller($scope){
-    $scope.imageData = "";
-    $scope.recTime = 0;
-     $scope.takePhoto = function(){
-        navigator.camera.getPicture(
-            function(imageData) {
-                $scope.$apply(function(){
-                    $scope.imageData = "data:image/jpeg;base64," + imageData;
-                });
-            }, function (message) {
-                alert('エラーが発生しました: ' + message);
-            }, {
-                quality: 50,
-                destinationType: Camera.DestinationType.DATA_URL
-            }
-        );
+  $scope.imageData = "";
+  $scope.recTime = 0;
+  
+  $scope.takePhoto = function(){
+    navigator.camera.getPicture(
+      function(imageData) {
+        $scope.$apply(function(){
+          savePhoto(imageData, new Date);
+        });
+      }, function (message) {
+        alert('エラーが発生しました: ' + message);
+      }, {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL
+      }
+    );
+  }
+  
+  db().transaction(
+    function(tx) {
+      tx.executeSql('SELECT * FROM Photos', [], function(tx, ret) {
+        $scope.$apply(function(){
+          $scope.photos = rowListAsArray(ret.rows);
+        });
+      });
+    },
+    function(err) {
+      alert("SQL 実行中にエラーが発生しました: "+err);
     }
-    $scope.recordVoice = function() {
-        var src = "myrecording.amr";
-        function onSuccess() {
-            console.log("recordAudio():Audio Success");
-        }
-        function onError(error) {
-            alert('コード: '        + error.code    + '\n' +
-                  'メッセージ: '    + error.message + '\n');
-        }
+  );
+}
 
+function rowListAsArray(rows) {
+  var arr = [];
+  for (var i =0; i < rows.length; ++i ){
+    arr.push(rows.item(i));
+  }
+  return arr;
+}
 
-        var mediaRec = new Media(src, onSuccess, onError);
-
-        // オーディオの録音
-        mediaRec.startRecord();
-
-        // 6秒後に録音を停止
-        var start = new Date;
-        var recInterval = setInterval(function() {
-            var now = new Date;
-            var duration = now.getTime() - start.getTime();
-            
-            $scope.$apply(function(){
-                $scope.recTime = duration;
-            });
-            
-            if (duration / 1000  >= 6) {
-                clearInterval(recInterval);
-                mediaRec.stopRecord();
-            }
-        }, 100);
-    };
-
-    
-    $scope.playVoice = function() {
-        var src = "myrecording.amr";
-          function onSuccess() {
-            console.log("recordAudio():Audio Success");
-        }
-        function onError(error) {
-            alert('コード: '        + error.code    + '\n' +
-                  'メッセージ: '    + error.message + '\n');
-        }
-
-        var media = new Media(src, onSuccess, onError);
-        media.play();
-    }
+function Page2Controller($scope){
 }
